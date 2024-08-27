97f800da897388768420161d9cbec38955b61ddb
Jan Lehigh <janlehigh461437@gmail.com>
2024-08-27 17:07:24 -0600

    Changes to the controller for markets

--- app/controllers/api/v0/markets_controller.rb
+++ app/controllers/api/v0/markets_controller.rb
@@ -5,6 +5,6 @@ class Api::V0::MarketsController < ApplicationController
   def show 
     markets = Market.all
 
-    render json: MarketSerializer.formate_markets(Market.find(params[:id])), status: :ok
+    render json: MarketSerializer.format_markets(markets), status: :ok
   end
 end
