097d96b1b59e251d6ec146e79762035a01eb5cc1
Jan Lehigh <janlehigh461437@gmail.com>
2024-08-27 17:25:45 -0600

    Added iteration for parsed json for markets

--- spec/requests/api/v0/get_one_market_spec.rb
+++ spec/requests/api/v0/get_one_market_spec.rb
@@ -6,43 +6,45 @@ RSpec.describe "Market Money API" do
     
     vendor = create(:vendor)
 
-    market_vendors = MarketVendor.create(market: id, vendor: vendor)
+    market_vendors = MarketVendor.create(market_id: id, vendor_id: vendor)
 
 
     get "/api/v0/markets/#{id}"
 
-    market = JSON.parse(response.body, symbolize_names: true)
+    markets = JSON.parse(response.body, symbolize_names: true)
 
     expect(response).to be_successful
-
-    expect(market).to have_key(:id)
-    expect(market[:id]).to eq(id)
-
-    expect(market).to have_key(:name)
-    expect(market[:name]).to be_a(String)
-
-    expect(market).to have_key(:street)
-    expect(market[:street]).to be_a(String)
-
-    expect(market).to have_key(:city)
-    expect(market[:city]).to be_a(String)
-
-    expect(market).to have_key(:county)
-    expect(market[:county]).to be_a(String)
-
-    expect(market).to have_key(:state)
-    expect(market[:state]).to be_a(String)
     
-    expect(market).to have_key(:zip)
-    expect(market[:zip]).to be_a(String)
+    markets.each do |market|
+      expect(market).to have_key(:id)
+      expect(market[:id]).to eq(id)
 
-    expect(market).to have_key(:lat)
-    expect(market[:lat]).to be_a(String)
+      expect(market).to have_key(:name)
+      expect(market[:name]).to be_a(String)
 
-    expect(market).to have_key(:lon)
-    expect(market[:lon]).to be_a(String)
-    
-    expect(market).to have_key(:vendor_count)
-    expect(market[:vendor_count]).to eq(1)
+      expect(market).to have_key(:street)
+      expect(market[:street]).to be_a(String)
+
+      expect(market).to have_key(:city)
+      expect(market[:city]).to be_a(String)
+
+      expect(market).to have_key(:county)
+      expect(market[:county]).to be_a(String)
+
+      expect(market).to have_key(:state)
+      expect(market[:state]).to be_a(String)
+      
+      expect(market).to have_key(:zip)
+      expect(market[:zip]).to be_a(String)
+
+      expect(market).to have_key(:lat)
+      expect(market[:lat]).to be_a(String)
+
+      expect(market).to have_key(:lon)
+      expect(market[:lon]).to be_a(String)
+      
+      expect(market).to have_key(:vendor_count)
+      expect(market[:vendor_count]).to eq(1)
+    end
   end
 end
