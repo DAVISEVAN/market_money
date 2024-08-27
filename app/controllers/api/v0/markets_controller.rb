d0e3f9b66babe0628540aa5698030a3b003ba0ef
Jan Lehigh <janlehigh461437@gmail.com>
2024-08-27 17:04:20 -0600

    Add changes to the json redner for the market serializer line in the show action

--- app/controllers/api/v0/markets_controller.rb
+++ app/controllers/api/v0/markets_controller.rb
@@ -5,6 +5,6 @@ class Api::V0::MarketsController < ApplicationController
   def show 
     markets = Market.all
 
-    render json: MarketSerializer.find(params[:id]), status: :ok
+    render json: MarketSerializer.formate_markets(Market.find(params[:id])), status: :ok
   end
 end
