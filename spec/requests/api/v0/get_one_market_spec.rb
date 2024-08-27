4a357aaf2fb91db873ccd12003f0843e19ddc7c5
Jan Lehigh <janlehigh461437@gmail.com>
2024-08-26 20:28:30 -0600

    Add basic test for get one market functionality

---
+++ spec/requests/api/v0/get_one_market_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe "Market Money API"
+  it "can get one market by its id" do
+    id = create(:market).id
+    
+    get "/api/v0/markets/#{id}"
+
+    market = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+
+    expect(market).to have_key(:id)
+    expect(market[:id]).to eq(id)
+
+    expect(market).to have_key(:name)
+    expect(market[:name]).to be_a(String)
+
+    expect(market).to have_key(:street)
+    expect(market[:street]).to be_a(String)
+
+    expect(market).to have_key(:city)
+    expect(market[:city]).to be_a(String)
+
+    expect(market).to have_key(:county)
+    expect(market[:county]).to be_a(String)
+
+    expect(market).to have_key(:state)
+    expect(market[:state]).to be_a(String)
+    
+    expect(market).to have_key(:zip)
+    expect(market[:zip]).to be_a(String)
+
+    expect(market).to have_key(:lat)
+    expect(market[:lat]).to be_a(String)
+
+    expect(market).to have_key(:lon)
+    expect(market[:lon]).to be_a(String)
+  end
+end
