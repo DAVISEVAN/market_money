c18dc420f02226cdb3e60ec50292810c152fba0e
Jan Lehigh <janlehigh461437@gmail.com>
2024-08-28 08:25:22 -0600

    Fix the formating for the market serializer

--- app/serializers/market_serializer.rb
+++ app/serializers/market_serializer.rb
@@ -3,7 +3,29 @@ class MarketSerializer
     {
       data: markets.map do |market|
         {
-          id: market.id,
+          type: "market",
+          attributes: {
+            name: market.name,
+            street: market.street,
+            city: market.city,
+            county: market.county,
+            state: market.state,
+            zip: market.zip,
+            lat: market.lat,
+            lon: market.lon,
+            vendor_count: market.vendors.count
+          }
+        }
+      end
+    }
+  end
+
+  def self.format_market(market)
+    {
+      data: {
+        id: market.id,
+        type: "market",
+        attributes: {
           name: market.name,
           street: market.street,
           city: market.city,
@@ -14,23 +36,6 @@ class MarketSerializer
           lon: market.lon,
           vendor_count: market.vendors.count
         }
-      end
-    }
-  end
-
-  def self.format_market(market)
-    {
-      data: {
-        id: market.id,
-        name: market.name,
-        street: market.street,
-        city: market.city,
-        county: market.county,
-        state: market.state,
-        zip: market.zip,
-        lat: market.lat,
-        lon: market.lon,
-        vendor_count: market.vendors.count
       }
     }
   end
