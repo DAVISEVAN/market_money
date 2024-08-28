897fa92e33906b9ae9bb307dc83e3d7c2ae6cac1
Jan Lehigh <janlehigh461437@gmail.com>
2024-08-28 08:30:21 -0600

    Remove superfulous method for market serializing

--- app/serializers/market_serializer.rb
+++ app/serializers/market_serializer.rb
@@ -1,24 +1,4 @@
 class MarketSerializer
-  def self.format_markets(markets)
-    {
-      data: markets.map do |market|
-        {
-          type: "market",
-          attributes: {
-            name: market.name,
-            street: market.street,
-            city: market.city,
-            county: market.county,
-            state: market.state,
-            zip: market.zip,
-            lat: market.lat,
-            lon: market.lon,
-            vendor_count: market.vendors.count
-          }
-        }
-      end
-    }
-  end
 
   def self.format_market(market)
     {
