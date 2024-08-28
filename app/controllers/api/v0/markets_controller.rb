778533cbe7822114897860b1f87dfc9cd6136320
Jan Lehigh <janlehigh461437@gmail.com>
2024-08-28 07:36:53 -0600

    Add render json using error serializer

--- app/controllers/api/v0/markets_controller.rb
+++ app/controllers/api/v0/markets_controller.rb
@@ -7,14 +7,8 @@ class Api::V0::MarketsController < ApplicationController
     begin
       render json: MarketSerializer.format_markets(markets)
     rescue ActiveRecord::RecordNotFound => exceptions
-      render json: {
-        errors: [
-          {
-            status: "404", 
-            title: exception.message
-          }
-        ]
-      }, status: :not_found
+      render json: ErrorSerializer.new(ErrorMessage.new(exception.message, 404))
+      .serialize_json, status: :not_found
     end
   end
 end
