9c83c02670d67d422eafee6cd0fad5b6c1c2d9c7
Jan Lehigh <janlehigh461437@gmail.com>
2024-08-28 07:47:52 -0600

    Add new serializer method

--- app/serializers/market_serializer.rb
+++ app/serializers/market_serializer.rb
@@ -16,6 +16,24 @@ class MarketSerializer
         }
       end
     }
-  
+  end
+
+  def self.format_market(market)
+    {
+      data:
+        {
+          id: market.id,
+          name: market.name,
+          street: market.street,
+          city: market.city,
+          county: market.county,
+          state: market.state,
+          zip: market.zip,
+          lat: market.lat,
+          lon: market.lon,
+          vendor_count: market.vendors.count
+        }
+      end
+    }
   end
 end
