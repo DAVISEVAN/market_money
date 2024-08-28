981a42fd696b4598e6d91300cb102b27f35be26e
Jan Lehigh <janlehigh461437@gmail.com>
2024-08-28 14:29:00 -0600

    Add empty infrastucture for the test

--- app/controllers/api/v0/markets_controller.rb
+++ app/controllers/api/v0/markets_controller.rb
@@ -12,6 +12,7 @@ class Api::V0::MarketsController < ApplicationController
     render json: { errors: [{ detail: "Couldn't find Market with 'id'=#{params[:id]}" }] }, status: :not_found
   end
 
+  def 
   # private
   
   # def not_found_response(exception)
---
+++ spec/requests/api/v0/search_markets_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe "Market Money API" do
+  before :each do
+    market1 = Market.create(id: )
+    market2 = Market.create(  )
+  end 
+
+  it "can get a market when searched by state, city, or name" do
+
+  end
+
+  it "cannot get a market when searched by city or name and city"
+
+  end
+end
