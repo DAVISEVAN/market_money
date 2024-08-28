a728bd7b75e1e71b98d0e72e73b7210256479952
Jan Lehigh <janlehigh461437@gmail.com>
2024-08-28 07:35:32 -0600

    Add changes to the market_serializer

--- app/serializers/market_serializer.rb
+++ app/serializers/market_serializer.rb
@@ -1,24 +1,21 @@
 class MarketSerializer
   def self.format_markets(markets)
-    markets.map do |market|
-      {
-        data: {
-
+    {
+      data: markets.map do |market|
+        {
+          id: market.id,
+          name: market.name,
+          street: market.street,
+          city: market.city,
+          county: market.county,
+          state: market.state,
+          zip: market.zip,
+          lat: market.lat,
+          lon: market.lon,
+          vendor_count: market.vendors.count
         }
-        id: market.id,
-        name: market.name,
-        street: market.street,
-        city: market.city,
-        county: market.county,
-        state: market.state,
-        zip: market.zip,
-        lat: market.lat,
-        lon: market.lon,
-        vendor_count: market.vendors.count
-        errors: {
-          
-        }
-      }
-    end
+      end
+    }
+  
   end
 end
