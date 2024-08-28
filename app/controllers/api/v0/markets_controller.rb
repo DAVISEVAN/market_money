b7ca350c0e9cd1e8737787e59b2ea048513eb7f6
Jan Lehigh <janlehigh461437@gmail.com>
2024-08-28 15:30:54 -0600

    Add search action to markets controller

--- app/controllers/api/v0/markets_controller.rb
+++ app/controllers/api/v0/markets_controller.rb
@@ -12,11 +12,7 @@ class Api::V0::MarketsController < ApplicationController
     render json: { errors: [{ detail: "Couldn't find Market with 'id'=#{params[:id]}" }] }, status: :not_found
   end
 
-  def 
-  # private
-  
-  # def not_found_response(exception)
-  #   render json: ErrorSerializer.new(ErrorMessage.new(exception.message, 404))
-  #     .serialize_json, status: :not_found
-  # end
+  def search
+
+  end
 end
