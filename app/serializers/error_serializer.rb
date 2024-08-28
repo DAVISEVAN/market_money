3fc8e425fe89f4a62794893b3adddfdfe84f0379
Jan Lehigh <janlehigh461437@gmail.com>
2024-08-28 07:38:34 -0600

    Add serialize_json method for the error serialilzer

--- app/serializers/error_serializer.rb
+++ app/serializers/error_serializer.rb
@@ -2,4 +2,15 @@ class ErrorSerializer
   def initialize(error_object)
     @error_object = error_object
   end
+
+  def serialize_json
+    {
+      errors: [
+        {
+          status: @error_object.status_code.to_s,
+          title: @error_object.message
+        }
+      ]
+    }
+  end
 end
