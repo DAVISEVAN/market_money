632f3d3d7c8ec85d8d7c39a58f7b5fae7e8019e0
Jan Lehigh <janlehigh461437@gmail.com>
2024-08-28 09:18:08 -0600

    Add changes to markets controller for not found response

--- app/controllers/api/v0/markets_controller.rb
+++ app/controllers/api/v0/markets_controller.rb
@@ -1,14 +1,17 @@
 class Api::V0::MarketsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
+
   def index
   end
 
   def show 
-    markets = Market.all
-    begin
-      render json: MarketSerializer.format_market(Market.find(params[:id]))
-    rescue ActiveRecord::RecordNotFound => exceptions
-      render json: ErrorSerializer.new(ErrorMessage.new(exception.message, 404))
+    render json: MarketSerializer.format_market(Market.find(params[:id]))
+  end
+
+  private
+  
+  def not_found_response(exception)
+    render json: ErrorSerializer.new(ErrorMessage.new(exception.message, 404))
       .serialize_json, status: :not_found
-    end
   end
 end
