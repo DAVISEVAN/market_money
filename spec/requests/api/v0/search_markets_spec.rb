53fbf503a7b5e309febc9f38996ede78d3c0f685
Jan Lehigh <janlehigh461437@gmail.com>
2024-08-28 14:36:06 -0600

    Add additional test setup

--- spec/requests/api/v0/search_markets_spec.rb
+++ spec/requests/api/v0/search_markets_spec.rb
@@ -2,15 +2,23 @@ require 'rails_helper'
 
 RSpec.describe "Market Money API" do
   before :each do
-    market1 = Market.create(id: )
-    market2 = Market.create(  )
-  end 
+    @market1 = Market.create(:market, city: 'Albuquerque', state: 'New Mexico', name: 'Nob Hill Growers Market' )
+    @market2 = Market.create(:market, city: 'Santa Fe', state: 'New Mexico', name: 'Santa Fe Market'  )
 
-  it "can get a market when searched by state, city, or name" do
+  describe "GET /api/v0/markets/search" do
+    context "when valid parameters are present" do
+      it "returns correct market" do
+        
+        get "/api/v0/markets/search"
 
-  end
+        
 
-  it "cannot get a market when searched by city or name and city"
+      end
+    end
 
+    context "when invalid parameters are sent" do
+      it "cannot get a market when searched by city or name and city"
+      end
+    end
   end
 end
