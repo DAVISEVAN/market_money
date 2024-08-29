e10483dab9317447f734f2b51aa2c25f78f94cc3
Evan Davis <evandavis427@gmail.com>
2024-08-29 07:40:54 -0600

    added credenials yml file for next us

--- spec/models/vendor_spec.rb
+++ spec/models/vendor_spec.rb
@@ -5,4 +5,13 @@ RSpec.describe Vendor, type: :model do
     it { should have_many(:market_vendors) }
     it { should have_many(:markets).through(:market_vendors) }
   end
+
+  describe 'instance_methods' do
+    it "returns an error if no credit card number is given" do
+      vendor = Vendor.new(credit_accepted: nil)
+
+      expect(vendor).not_to be_valid 
+      expect(vendor.errors[:credit_accepted]).to include("can't be blank")
+    end
+  end
 end
